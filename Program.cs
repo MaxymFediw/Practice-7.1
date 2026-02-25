724106f9685d312956f1d6f12468618761916b14
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-25 13:13:12 -0500

    Checkpoint #1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Part 1, twin!
+
+            Part1();
+
+
         }
-    }
+
+        public static void Part1() 
+        {
+
+            int pin = 12345;
+            int entry;
+            Console.WriteLine("WELCOME TO THE BANK OF ALDWORTH.");
+            Console.Write("ENTER YOUR PIN: ");
+            
+            int.TryParse(Console.ReadLine(), out entry);
+            Console.WriteLine();
+            while (entry != pin)
+            {
+                Console.WriteLine("INCORRECT PIN. TRY AGAIN.");
+                Console.Write("ENTER YOUR PIN: ");
+                int.TryParse(Console.ReadLine(), out entry);
+                Console.WriteLine();
+            }
+            Console.WriteLine("PIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+
+
+
+        }
+    }   
+
+        
 }
